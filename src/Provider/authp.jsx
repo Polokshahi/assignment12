da8175bfc75a29dc1a5615e7d1f7c2b62d157a5b
Polok Shahi <polokshahi2@gmail.com>
2025-04-29 10:17:50 +0600

    done

---
+++ src/Provider/authp.jsx
@@ -0,0 +1,13 @@
+import { createContext } from "react";
+
+const authContext = createContext(null); //null is the default value
+
+const AuthProvider = ({children}) => {
+    return (
+        <authContext.Provider value={{}}>
+            {children}
+        </authContext.Provider>
+    )
+}
+
+export default AuthProvider;
