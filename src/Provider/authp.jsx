ecff2307167943952d7bbffec02d3e50548991a2
Polok Shahi <polokshahi2@gmail.com>
2025-04-29 10:18:39 +0600

    Build destination cards component

--- src/Provider/authp.jsx
+++ src/Provider/authp.jsx
@@ -4,6 +4,4 @@ const authContext = createContext(null); //null is the default value
 
 const AuthProvider = ({children}) => {
     return (
-        <authContext.Provider value={{}}>
-            {children}
-        </authContext.Provider>
+
